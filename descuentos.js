752edf21b1a1c099d98c38876b4688a62fb2de42
ramon_puon <ramon.puon@gmail.com>
2021-10-06 10:13:48 -0500

    Coupons

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,21 @@
 //const precioOriginal = 120;
 //const descuento = 18;
 
+const coupons = [
+    {
+        name: "Platzi 1",
+        discount: 15,
+    },
+    {
+        name: "Platzi 2",
+        discount: 30,
+    },
+    {
+        name: "Platzi 3",
+        discount: 25,
+    },
+];
+
 function calcularPrecioDescuento(precio,descuento){
     const porcentajePrecioDescuento = 100 - descuento;
     const precioConDescuento = (precio*porcentajePrecioDescuento)/100;
@@ -16,9 +31,39 @@ function onClickButtonDiscount(){
     const inputdiscount = document.getElementById("input-discount");
     const discountValue = inputdiscount.value;
 
-    const precioConDescuento = calcularPrecioDescuento(priceValue, discountValue);
+    if (discountValue != 0){
+        const precioConDescuento = calcularPrecioDescuento(priceValue, discountValue);
 
-    const resultP = document.getElementById("ResultP");
-    resultP.innerText = "El precio con descuento son: $"+precioConDescuento
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $"+precioConDescuento
+    }else {
+        const inputCoupon = document.getElementById("input-coupon");
+        const couponValue = inputCoupon.value;
+
+        //Compare couponValue with coupon array name
+        const isCouponValid = function(coupon){
+            return coupon.name === couponValue;
+        }
+
+        //Buscar si cualquier name es igual a couponValue
+        const userCoupon = coupons.find(isCouponValid);
+
+        if(!userCoupon){
+            alert("El cupón "+couponValue+" no es válido")
+        } else{
+            const descuento = userCoupon.discount;
+
+            const precioConDescuento = calcularPrecioDescuento(priceValue, descuento);
+
+            const resultP = document.getElementById("ResultP");
+            resultP.innerText = "El precio con descuento son: $"+precioConDescuento
+
+
+        }
+    }
+
+   
 }
+
+
 
