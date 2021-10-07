030f2b1213bd5e739aae6f391c7e478648ba031d
ramon_puon <ramon.puon@gmail.com>
2021-10-07 09:39:48 -0500

    mediana top 10

--- analisis.js
+++ analisis.js
@@ -1,3 +1,45 @@
+//Helpers
+
+function esPar(numero){
+    return (numero % 2 ===0);
+}
+
+function calcularMediaAritmetica(lista){
+    //let , la variable se puede cambiar
+    //let sumaLista = 0;
+
+    //for (let i = 0; i < lista.length; i++)
+    
+   // sumaLista = sumaLista + lista[i];
+    
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promediolista = sumaLista/lista.length;
+    return promediolista;
+}
+
+//Calculadora Mediana
+function medianaSalarios(lista){
+    const mitad = parseInt(lista.length/2);
+
+    if (esPar(lista.length)){
+        const personaMitad1 = lista[mitad-1];
+        const personaMitad2 = lista[mitad];
+        const mediana = calcularMediaAritmetica([personaMitad1,personaMitad2]);
+        return mediana
+
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+}
+
+//Mediana General
 const salariosCol = colombia.map(
     function(persona){
         return persona.salary;
@@ -5,25 +47,24 @@ const salariosCol = colombia.map(
 );
 
 const salariosColSorted = salariosCol.sort(
-    function(salaryA, salatyB){
-        salaryA - salatyB;
+    function(salaryA, salaryB){
+        return salaryA - salaryB;
     }
 );
 
-function esPar(numero){
-    return (numero % 2 ===0);
-}
 
-function medianaSalarios(lista){
-    const mitad = parseInt(lista.length/2);
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
 
-    if (esPar(lista.length)){
-        const personaMitad1 = lista[mitad]-1;
-        const personaMitad2 = lista[mitad];
-        const mediana = (personaMitad1+personaMitad2)/2;
+//Mediana del top 10%
 
-    } else {
-        const personaMitad = lista[mitad];
-        return personaMitad;
-    }
-}
+const spliceStart = (salariosColSorted.length * 0.9);
+const spliceCount = salariosColSorted.length - spliceStart;
+//Se creó un array con las últimas 10 personas del array de salarios (El top 10% de salarios)
+const salariosColTop10 = salariosColSorted.splice(spliceStart, spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
